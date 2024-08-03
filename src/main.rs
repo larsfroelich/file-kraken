02dbf6a63f25d8aeb0fc1f942841d4bbc4f23f48
Lars Frölich <lars.froelich@gmail.com>
2024-08-03 15:49:51 +0200

    added basic egui example project

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![allow(rustdoc::missing_crate_level_docs)] // it's an example
+
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    // init env logger
+    env_logger::init();
+
+    // Run the app
+    eframe::run_native(
+        "File Kraken",
+        eframe::NativeOptions {
+            viewport: egui::ViewportBuilder::default().with_maximized(true),
+            ..Default::default()
+        },
+        Box::new(|_cc| Ok(Box::<MyApp>::default())),
+    )
+}
+
+#[derive(Default)]
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("File Kraken");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+
+        });
+    }
+}
