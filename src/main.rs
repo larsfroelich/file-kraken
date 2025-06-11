f012934e34e03930709ba3755f214116827f3969
Lars Frölich <lars.froelich@gmail.com>
2025-06-11 14:28:07 +0200

    hotfix

--- src/main.rs
+++ src/main.rs
@@ -55,6 +55,7 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 impl eframe::App for FileKrakenApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         std::thread::sleep(Duration::from_micros((1.0 / 120.0 * 1_000_000.0) as u64));
+        egui::CentralPanel::default().show(ctx, |ui| {
             if !self.app_state.is_sqlite_connected() {
                 ui.centered_and_justified(|ui| {
                     ui.vertical_centered(|ui| {
@@ -112,19 +113,15 @@ impl eframe::App for FileKrakenApp {
                         RichText::from(String::from(" v") + VERSION)
                             .font(FontId::proportional(20.0)),
                     );
-                    ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
-                        if ui
-                            .button(if self.dark_theme { "☀️" } else { "🌙" })
-                            .clicked()
-                        {
-                            self.dark_theme = !self.dark_theme;
-                            set_theme(ctx, if self.dark_theme { MOCHA } else { LATTE });
-                        }
-                    });
+                    if ui
+                        .button(if self.dark_theme { "☀️" } else { "🌙" })
+                        .clicked()
+                    {
+                        self.dark_theme = !self.dark_theme;
+                        set_theme(ctx, if self.dark_theme { MOCHA } else { LATTE });
+                    }
                 },
             );
-                }
-            });
 
             ui.add_space(10.0);
             ui.horizontal(|ui| {
